b938ba0e6dd19320702371da6f902076abca101b
Derek <DerekPistorius@devmounta.in>
2021-11-22 20:21:47 -0800

    initial commit for sldc

---
+++ sldc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing and Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+var Planning = "In this phase we discuss what are we trying to build, who might tackle specific parts of the project, and potential deadlines"
+var AnalysisOfRequirements = "In this phase we discuss how do we execute our plan and what specific things need to be included to make our project work as intended"
+var Design = "In this phase we discuss what the project looks like appearance wise and how the user interacts with it"
+var Implementation = "This is the coding phase"
+var Testingandintegration = "During this phase we bring all the seperate pieces together and run tests to ensure everything is working as intended"
+var Maintain = "We perform maintanence on our project to remove bugs and make changes to our project with updates."
+
+
+console.log(Planning)
+console.log(AnalysisOfRequirements)
+console.log(Design)
+console.log(Implementation)
+console.log(Testingandintegration)
+console.log(Maintain)
